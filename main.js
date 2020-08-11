160dfab03a3a75231138c981d63a5e72ccbead09
mikail karadoğan <68776400+mikail-karadogan@users.noreply.github.com>
2020-08-11 19:47:34 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 function photo(param) {
-    // if (param == cat) {
-    //     document.getElementById("img").src="https://random.dog/woof.json";
-    // }if (param == dog) {
-    //     document.getElementById("img").src="https://random.dog/woof.json";
-    // }
      fetch("https://random.dog/woof.json")
      .then(response => response.json())
      .then(data => (document.getElementById("img").src = data.url) );
